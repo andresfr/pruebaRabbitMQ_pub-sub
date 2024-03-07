7f96c8090535aa0bbbd5591d824f9e239a92a3a6
Andres Ramirez <andresfr@gmail.com>
2024-03-06 22:46:26 -0500

    Corrección de los nombramientos de las colas en el momento de la creación

--- pub/Program.cs
+++ pub/Program.cs
@@ -6,19 +6,21 @@ var factory = new ConnectionFactory {     HostName = "localhost",     UserName =
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "hello2",
+channel.QueueDeclare(//queue: "hello2",
                      durable: true,
                      exclusive: false,
                      autoDelete: false,
                      arguments: null);
 
-const string message = "prueba lunes medio dia 4";
+const string message = "prueba martes andres ";
+
 var body = Encoding.UTF8.GetBytes(message);
 
 channel.BasicPublish(exchange: string.Empty,
                      routingKey: "hello2",
                      basicProperties: null,
                      body: body);
+
 Console.WriteLine($" [x] Sent {message}");
 
 Console.WriteLine(" Press [enter] to exit.");
